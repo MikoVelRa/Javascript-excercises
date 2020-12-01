e57b8a691e87442fbb61c80f3ccaeacf415d7f2d
Miguel Arturo Velasco Ramírez <devdayarturo@gmail.com>
2020-12-01 16:20:09 -0600

    Validación en métodos factoriales y <<esCapicua()>>

--- ejercicios3.js
+++ ejercicios3.js
@@ -1,9 +1,12 @@
-//Forma difícil (sin conversión del número a string u objeto)
+//Forma difícil (sin conversión del número a string u objeto, solo para números enteros)
 
 const getMaxDivisor = (number) => {
   if (number < 10) return "Defina un número mayor o igual a 10";
 
-  let maxDivisor, j = 1, capicuo = 0, contador = number;
+  let maxDivisor,
+    j = 1,
+    capicuo = 0,
+    contador = number;
 
   for (let i = 1; i <= number; i *= 10) maxDivisor = i;
 
@@ -21,33 +24,38 @@ const getMaxDivisor = (number) => {
 
 const esCapicua = (number) => {
   if (number < 10) return "Defina un número mayor o igual a 10";
-    return parseInt(number.toString().split("").reverse().join("")) === number
-      ? "es capicúa"
-      : "no es capicúa";
+  return number.toString().split("").reverse().join("") == number
+    ? "es capicúa"
+    : "no es capicúa";
 };
 
 //Random
 
-const getRandom = (inicio, final) => Math.round(Math.random() * (inicio - final) + final);
+const getRandom = (inicio, final) =>
+  Math.round(Math.random() * (inicio - final) + final);
 
 //Factorial recursivo
-const factorial = number => {
-  if(number === 1) return 1;
-  else return number * factorial(number-1);
-}
+const factorial = (number) => {
+  if (number === 1) return 1;
+  else if (number === 0) return "El valor no puede ser 0";
+  else if (Math.sign(number) === -1) return "El valor no puede ser negativo";
+  else if (typeof number !== "number") return "El valor debe ser un número";
+  else return number * factorial(number - 1);
+};
 
 //Factorial iterativo
-const facIterativo = number => {
-  if(number === 1) return 1;
-  for(let i = number; i > 1; i--){
-    number*= i-1;
-  }
+const facIterativo = (number) => {
+  if (number === 1) return 1;
+  else if (number === 0) return "El valor no puede ser 0";
+  else if (Math.sign(number) === -1) return "El valor no puede ser negativo";
+  else if (typeof number !== "number") return "El valor debe ser un número";
+  for (let i = number; i > 1; i--) number *= i - 1;
   return number;
-}
+};
 
 //Pruebas
-console.log(getMaxDivisor(2002));
-console.log(esCapicua(2002));
+console.log(getMaxDivisor(212.212));
+console.log(esCapicua(212.212));
 console.log(getRandom(501, 600));
 console.log(factorial(5));
 console.log(facIterativo(5));
